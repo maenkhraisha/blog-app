d0a5da8aa70c0ebc3df2c27dbcfaafa0bf5504f1
maen <maenkh@yahoo.com>
2022-09-15 21:01:06 +0300

    fix linter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_save :update_user_posts_count
+
+  def update_user_posts_count
+    author.increment!(:posts_counter)
+  end
+
+  def recent_five_comments
+    comments.last(5)
+  end
 end
