f4535adf62137ab60eec3981527fdd73d873faf1
maen <maenkh@yahoo.com>
2022-09-16 18:56:09 +0300

    finish the feedback comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
 
   after_save :update_user_posts_count
 
@@ -7,6 +9,8 @@ class Post < ApplicationRecord
     author.increment!(:posts_counter)
   end
 
+  private
+  
   def recent_five_comments
     comments.last(5)
   end
