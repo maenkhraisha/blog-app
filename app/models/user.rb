afa07c66b251fa597fb6541806cac0619536d553
maen <maenkh@yahoo.com>
2022-09-16 18:26:53 +0300

    fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
-  
-    def recent_three_post
-      posts.limit(3).order(created_at: :desc)
-    end
+
+  def recent_three_post
+    posts.limit(3).order(created_at: :desc)
+  end
 end
