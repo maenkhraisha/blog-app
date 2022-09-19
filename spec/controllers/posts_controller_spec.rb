3b1f348f5d0dc819385ddfde2eb4bb21e6f10594
maen <maenkh@yahoo.com>
2022-09-19 19:42:28 +0300

    refactor code

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
-  describe 'POST GET index' do
+  describe 'POST / GET index' do
+    before(:example) do
+      get '/users/1/posts'
+    end
+
     it 'returns a 200' do
-      get user_posts_path(user_id: 22, id: 50)
       expect(response).to have_http_status(200)
     end
 
     it 'renders the index template' do
-      get user_posts_path(user_id: 22, id: 50)
       expect(response).to render_template('index')
     end
 
     it 'response body includes correct placeholder text' do
-      get user_posts_path(user_id: 22, id: 50)
       response.body = 'Posts Index'
     end
   end
-end
 
-RSpec.describe PostsController, type: :request do
-  describe 'POST GET show' do
+  describe 'POST / GET show' do
+    before(:example) do
+      get '/users/1/posts/1'
+    end
+
     it 'returns a 200' do
-      get user_post_path(user_id: 22, id: 50)
       expect(response).to have_http_status(200)
     end
 
     it 'renders the index template' do
-      get user_post_path(user_id: 22, id: 50)
       expect(response).to render_template('show')
     end
 
     it 'response body includes correct placeholder text' do
-      get user_post_path(user_id: 22, id: 50)
       response.body = 'Posts Index'
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-  describe 'USER GET index' do
+RSpec.describe UsersController, type: :request do
+  describe 'USER / GET index' do
+    before(:example) do
+      get '/users'
+    end
+
     it 'returns a 200' do
-      get :index
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the index template' do
-      get :index
       expect(response).to render_template('index')
     end
 
     it 'response body includes correct placeholder text' do
-      get :index
       response.body = 'This is just a list of users'
     end
   end
-end
 
-RSpec.describe UsersController, type: :request do
-  describe 'USER GET Show' do
+  describe 'USER / GET Show' do
+    before(:example) do
+      get '/users/1'
+    end
+
     it 'returns a 200' do
-      get user_path(user_id: 2, id: 2)
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the show template' do
-      get user_path(user_id: 2, id: 2)
       expect(response).to render_template('show')
     end
 
     it 'response body includes correct placeholder text' do
-      get user_path(user_id: 2, id: 2)
       response.body = 'This is just a list of users'
     end
   end
