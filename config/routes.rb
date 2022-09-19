07d21da280384712530da7293b5f5646e8a63f76
maen <maenkh@yahoo.com>
2022-09-19 12:19:47 +0300

    create users and posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :games
+  resources :users
+  resources :posts
+  resources :likes
+  resources :comments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
