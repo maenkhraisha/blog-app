a822e3ef78e036ba7ce6c1294755d2eeac7bf995
maen <maenkh@yahoo.com>
2022-09-24 23:50:16 +0300

    finish add post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,30 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new    
+  end
+
+  def create    
+    @user = User.create(user_params)
+
+    redirect_to users_path
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit( :name, :photo, :bio,:posts_counter).with_defaults(posts_counter: 0)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root "users#index"
-  resources :users, only: [:index, :show] do
+  root "users#index" # for default route
+  resources :posts, only: [:create, :new ]
+  resources :users, only: [:index, :show, :new, :create, :destroy] do
     resources :posts, only: [:index, :show]
   end
 end
