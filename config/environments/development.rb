2f2ae9160c477f8a8cff17443a9d4d8fba0dc725
NatiDeme <demelashnatnael@gmail.com>
2022-09-29 16:51:06 +0300

    Add mailcatcher for confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
-  config.action_mailer.raise_delivery_errors = false
+
 
   # Settings specified here will take precedence over those in config/application.rb.
 
