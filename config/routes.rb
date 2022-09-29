77aeb6ec6ddc09fc41e5168064cffe61f2f5a518
maen <maenkh@yahoo.com>
2022-09-29 16:26:46 +0300

    try to install mailcatcher

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   root "users#index" # for default route
+  
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
+  
   resources :posts, only: [:create, :new ]
   resources :users, only: [:index, :show, :new, :create, :destroy] do
     resources :posts, only: [:index, :show] do
